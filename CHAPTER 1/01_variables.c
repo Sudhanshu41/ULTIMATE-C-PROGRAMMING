713cca98c9fc02799b07017904fbe1865457e958
Sudhanshu41 <skhoney05@gmail.com>
2024-10-21 16:44:03 +0600

    Initial commit

---
+++ CHAPTER 1/01_variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a; //variable declaration
+    a=6; //variable intilization
+    // int harry (this is allowed)
+    // int harry is sweet (this os not allowed bcz there cant be wide space character in variables)
+
+   printf("The output of this programm is %d", a); //pura same aayega bas jis jgh %d hai us jagah a ki value aajayegi.
+    return 0;
+
+}
