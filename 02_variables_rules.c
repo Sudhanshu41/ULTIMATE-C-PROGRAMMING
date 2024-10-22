4027a23d50bb0850a11df164dfed9c7eaa979d69
Sudhanshu41 <skhoney05@gmail.com>
2024-10-22 22:03:27 +0600

    Add files via upload

---
+++ 02_variables_rules.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int harry;
+int harry_good;
+int Harry;
+printf("%d",harry); // isko agar hum run karenge to output mai koi bhi random value aa jati hai like 250458
+
+return 0;
+
+}
