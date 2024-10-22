7e4aa0ed445fed92c901b1641d28008456b7f2ee
sudhanshu kumar <skhoney05@gmail.com>
2024-10-22 22:06:01 +0600

    Add files via upload

---
+++ 03_data_types.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+
+
+//int a;
+//a=1;
+int a = 1; // this is also valid like above
+
+//float b = 1.5;
+float b;
+b = 1.5; //(both are same)
+
+
+//The memory size for data types can vary depending on the system and compiler, but here are typical sizes in C/C++:
+//int: Usually 4 bytes (32 bits)
+//char: 1 byte (8 bits)
+//float: Usually 4 bytes (32 bits)
+//These sizes can differ on some systems, especially with different compilers or architectures.
+// For example, on some platforms, an int could be 2 bytes (16 bits), and a float could also vary.
+// Always refer to your specific environment's documentation for precise information.    51.22
+
+return 0;
+
+
+
+
+
+
+
+
+}
+
+
+
